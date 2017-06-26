e36a0f8cb5c94445a0f35771e93d15249685c5ac
n7nix <basil@pacabunga.com>
2017-06-26 09:30:38 -0700

    Initial commit

---
+++ k7ve/init.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import time
+import serial
+
+# configure the serial connections (the parameters differs on the device you are connecting to)
+ser = serial.Serial(
+    port='/dev/ttyS0',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+
+ser.isOpen()
+
+input = 'AT+DMOCONNECT'
+ser.write(input + '\r\n')
+print input
+line = ser.readline()
+print line
+
+input = "AT+DMOSETGROUP=0,144.3900,144.3900,0000,4,0000"
+ser.write(input + '\r\n')
+print input
+line = ser.readline()
+print line
+
+input = "AT+SETFILTER=1,1,1"
+ser.write(input + '\r\n')
+print input
+line = ser.readline()
+print line
+
+input = "AT+DMOSETVOLUME=3"
+ser.write(input + '\r\n')
+print input
+line = ser.readline()
+print line
